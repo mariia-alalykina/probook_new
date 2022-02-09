b377bcb3ac93f929d98b9f944f462486fa5a1234
mariia-alalykina <alal.masha2001@gmail.com>
2022-02-09 22:16:32 +0200

    Created main routes

--- app.js
+++ app.js
@@ -15,17 +15,40 @@ const jsonParser = express.json();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(request, response){
-     
-    // отправляем ответ
     response.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/about", function(request, response){
-     
-    // отправляем ответ
     response.sendFile(__dirname + "/public/about.html");
 });
 
+app.get("/basket", function(request, response){
+    response.sendFile(__dirname + "/public/basket.html");
+});
+
+app.get("/blog", function(request, response){
+    response.sendFile(__dirname + "/public/blog.html");
+});
+
+app.get("/catalog", function(request, response){
+    response.sendFile(__dirname + "/public/catalog.html");
+});
+
+app.get("/contacts", function(request, response){
+    response.sendFile(__dirname + "/public/contacts.html");
+});
+
+app.get("/delivery", function(request, response){
+    response.sendFile(__dirname + "/public/delivery.html");
+});
+
+app.get("/login", function(request, response){
+    response.sendFile(__dirname + "/public/login.html");
+});
+
+app.get("/news", function(request, response){
+    response.sendFile(__dirname + "/public/news.html");
+});
 
 
 app.listen(3000, function() {
